1f139ef7da3dc0d35e4388bb261eb67508e55741
supanatm <spnstation8@gmail.com>
2023-04-19 09:52:17 +0700

    edit table head font weight

--- src/Owner.jsx
+++ src/Owner.jsx
@@ -7,7 +7,7 @@ const Owner = () => {
             <div className='own-container'>
                 <div className='own-top'>
                     <h1>Aim (M) - Group C - 48</h1>
-                    <img src="https://images.unsplash.com/photo-1585507252242-11fe632c26e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
+                    <img src="https://images.unsplash.com/photo-1585507252242-11fe632c26e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="crazy" />
                 </div>
                 <div className='own-bottom'>
                     <h3>Short Biogragphy:</h3>
